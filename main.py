e1261f4cc8554c78dbb3562554c029d05880f147
YoanDemerdzhiev <stickno18yt@gmail.com>
2025-09-17 08:51:52 +0300

    New commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import uvicorn
 import os
 
 async def start_bot():
-    await bot.start(os.getenv("MTQxNzIwMjI4MTA5MDM4ODEzMg.Ge32g-.ds_Ws5_So8d1RpDyXajRorl9g1EFjoeOVOrb9E"))
+    await bot.start(os.getenv("MTQxNzIwMjI4MTA5MDM4ODEzMg.GJZNfy.PN6vdG-LupcX7hUD_Qdxfzv_XPyEtqatTptz3w"))
 
 async def start_server():
     config = uvicorn.Config(app, host="0.0.0.0", port=8000)
